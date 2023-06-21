1543da579cfb984bf8c5ebd6de4f5ce44a974569
pfenningpat <pat.pfenning@blastpoint.com>
2023-06-21 18:44:32 -0400

    get binary key

--- audio_encrypter/audio_encrypter.py
+++ audio_encrypter/audio_encrypter.py
@@ -1,14 +1,10 @@
 from pathlib import Path
 import numpy as np
+import pandas as pd
 from os import getenv
 from reader import read_wav
-from chaos import (
-    sim_chaotic_attractor,
-    henon,
-    lorenz,
-    ikeda,
-    logistic
-)
+from chaos import get_key
+from conversion import dec_to_bin
 from dotenv import load_dotenv
 
 env_file = Path("../config/.env")
@@ -28,5 +24,8 @@ def get_speaker_file(speaker_id: int, section_no: int) -> Path:
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     rate, data = read_wav(get_speaker_file(340, 340))
-    sol = sim_chaotic_attractor(logistic, [0.1], np.linspace(0, 4, data.shape[0]))
+    bin_df = pd.DataFrame(dec_to_bin(data), columns=['audio'])
+    soln = get_key(data.shape[0], np.random.random(2), np.random.random(2), np.random.random(3), np.random.random(1))
+    for n, col in enumerate(soln.T):
+        bin_df[f"key{n}"] = dec_to_bin(col)
 
--- audio_encrypter/chaos.py
+++ audio_encrypter/chaos.py
@@ -1,8 +1,10 @@
 from scipy.integrate import odeint
+from conversion import dec_to_bin
 import numpy as np
 
 
-def sim_chaotic_attractor(func, v0, t, **kwargs):
+def sim_chaotic_attractor(func, n, v0, **kwargs):
+    t = np.linspace(0, 1, n)
     return odeint(func, v0, t, tuple(kwargs))
 
 
@@ -26,3 +28,15 @@ def ikeda(v0, t, mu=0.7, beta=0.4, gamma=6):
 
 def logistic(v0, t, r=4):
     return r * v0[0] * (1 - v0[0])
+
+
+def __transform(data):
+    return np.mod(np.floor(np.abs(data) * 10 ** 10), 256).astype(np.int16)
+
+
+def get_key(n, henon_0, ikeda_0, lorenz_0, logistic_0):
+    sol_henon = sim_chaotic_attractor(henon, n, henon_0)
+    sol_ikeda = sim_chaotic_attractor(ikeda, n, ikeda_0)
+    sol_lorenz = sim_chaotic_attractor(lorenz, n, lorenz_0)
+    sol_logistic = sim_chaotic_attractor(logistic, n, logistic_0)
+    return __transform(np.concatenate((sol_henon, sol_ikeda, sol_lorenz, sol_logistic), axis=1))
