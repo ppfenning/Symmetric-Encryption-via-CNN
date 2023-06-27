a00b12bda966a8a7eef3b7d848d80660a11d5c72
pfenningpat <pat.pfenning@blastpoint.com>
2023-06-27 00:26:52 -0400

    combine to one array in encryption

--- audio_encrypter/encryption.py
+++ audio_encrypter/encryption.py
@@ -10,8 +10,8 @@ from chaos import (
 )
 
 
-def __transform_encryption(data):
-    return np.bitwise_xor.reduce(np.mod(np.floor(np.abs(data) * 10 ** 10), 2**16).astype(np.uint16), axis=1)
+def __transform(data):
+    return np.mod(np.floor(np.abs(data) * 10 ** 10), 2**16).astype(np.uint16)
 
 
 def get_key(keypath: Path, throw_away):
@@ -34,13 +34,14 @@ def get_key(keypath: Path, throw_away):
 
 def get_encryption(audio, *, throw_away, henon_0, ikeda_0, lorenz_0, logistic_0):
     file_len = audio.size + throw_away
-    encryption = __transform_encryption(np.concatenate((
+    encryption = __transform(np.concatenate((
         sim_chaotic_attractor(henon, file_len, henon_0),
         sim_chaotic_attractor(ikeda, file_len, ikeda_0),
         sim_chaotic_attractor(lorenz, file_len, lorenz_0),
         sim_chaotic_attractor(logistic, file_len, logistic_0)
     ), axis=1))[throw_away:]
-    return np.bitwise_xor.reduce(np.array([audio, encryption], dtype=np.uint16)).astype(np.int16)
+    combo = np.append(audio[:, np.newaxis], encryption, axis=1).astype(np.uint16)
+    return np.bitwise_xor.reduce(combo, axis=1).astype(np.int16)
 
 
 
